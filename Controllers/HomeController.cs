44f7b7dc98a1de955136daf49d7a7bbf6d7873cf
Michael Ludick <michael.ludick13@outlook.com>
2024-07-01 13:22:07 +0200

    Added  Conection string and controllers and views for bug through the lazy way auto generated code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Ticketing_System_Interview_Exam.Data;
 using Ticketing_System_Interview_Exam.Models;
 
 namespace Ticketing_System_Interview_Exam.Controllers
@@ -7,7 +8,7 @@ namespace Ticketing_System_Interview_Exam.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        private readonly Ticketing_System_Interview_ExamContext _context;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -21,6 +22,12 @@ namespace Ticketing_System_Interview_Exam.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public Task<IActionResult> QA_Index()
+        {
+            BugsController bugsController = new BugsController(_context);
+            return bugsController.Index();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Data/Ticketing_System_Interview_ExamContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ticketing_System_Interview_Exam.Models;
+
+namespace Ticketing_System_Interview_Exam.Data
+{
+    public class Ticketing_System_Interview_ExamContext : DbContext
+    {
+        public Ticketing_System_Interview_ExamContext (DbContextOptions<Ticketing_System_Interview_ExamContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ticketing_System_Interview_Exam.Models.Bug> Bug { get; set; } = default!;
+    }
+}
