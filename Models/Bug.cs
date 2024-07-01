99d410ba3da96f273f2e74664e63c0eb550a8e9b
Michael Ludick <michael.ludick13@outlook.com>
2024-07-01 13:08:27 +0200

    Added Models

---
+++ Models/Bug.cs
@@ -0,0 +1,12 @@
+﻿namespace Ticketing_System_Interview_Exam.Models
+{
+    public class Bug
+    {
+        public int BugId { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; } = "Open"; // Open or Resolved
+        public int CreatedByUserId { get; set; }
+
+    }
+}
